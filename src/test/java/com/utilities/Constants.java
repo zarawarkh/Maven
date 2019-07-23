ca6ed6b1145e05b49c29ce74d2e39ebe6fb8c8bc
zarawarf <zarawar02@gmail.com>
2019-07-22 21:55:22 -0400

    changes to Orange

---
+++ src/test/java/com/utilities/Constants.java
@@ -0,0 +1,9 @@
+package com.utilities;
+
+public class Constants {
+
+	public static final String filePath="src/test/resources/configs/credentilas.properties";
+	
+	public static String osName=System.getProperty("os.name");
+	
+}
